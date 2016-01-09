0f7fc8fb28cf80b077a756361476a2c6ffbfe1d1
Matthieu Napoli <matthieu@mnapoli.fr>
2016-01-09 19:07:01 +0100

    Require a module list to be given, drop the auto-loading of config using Puli bindings

--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -5,8 +5,6 @@ namespace DI\Kernel\Test;
 use DI\Kernel\Kernel;
 use DI\Kernel\Test\Fixture\PuliFactoryClass;
 use Puli\Discovery\Api\Discovery;
-use Puli\Discovery\Api\Type\BindingType;
-use Puli\Discovery\Binding\ResourceBinding;
 use Puli\Discovery\InMemoryDiscovery;
 use Puli\Repository\Api\ResourceRepository;
 use Puli\Repository\InMemoryRepository;
@@ -58,26 +56,16 @@ class KernelTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function registers_module_configuration_files()
+    public function loads_module_configs()
     {
-        $this->createPuliResource('/blog/config.php', __DIR__.'/Fixture/config.php');
-        $this->bindPuliResource('/blog/config.php', Kernel::PULI_BINDING_NAME);
+        PuliFactoryClass::$repository->add('/blog/config/config.php', new FileResource(__DIR__.'/Fixture/config.php'));
 
+        $this->kernel = new Kernel([
+            'blog',
+        ]);
+        $this->kernel->setPuliFactoryClass(PuliFactoryClass::class);
         $container = $this->kernel->createContainer();
+
         $this->assertEquals('bar', $container->get('foo'));
-    }
-
-    private function createPuliResource($path, $file)
-    {
-        PuliFactoryClass::$repository->add($path, new FileResource($file));
-    }
-
-    private function bindPuliResource($path, $bindingName)
-    {
-        PuliFactoryClass::$discovery->addBindingType(new BindingType($bindingName));
-
-        $binding = new ResourceBinding($path, $bindingName);
-        $binding->setRepository(PuliFactoryClass::$repository);
-        PuliFactoryClass::$discovery->addBinding($binding);
     }
 }
