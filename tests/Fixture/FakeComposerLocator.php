048bdc401e48a8970f211be55a528702cdba5c3c
Matthieu Napoli <matthieu@mnapoli.fr>
2017-04-09 21:14:00 +0200

    Simplify tests

--- tests/Fixture/FakeComposerLocator.php
+++ tests/Fixture/FakeComposerLocator.php
@@ -2,47 +2,14 @@
 
 namespace DI\Kernel\Test\Fixture;
 
-use RuntimeException;
-
 class FakeComposerLocator extends \ComposerLocator
 {
     public static $paths = [];
 
     public static $rootPath;
 
-    public static function getPath($name)
-    {
-        $name = strtolower($name);
-        if (!isset(self::$paths[$name])) {
-            throw new RuntimeException("Composer package not found: {$name}");
-        }
-
-        return self::getRootPath().self::$paths[$name];
-    }
-
     public static function getRootPath()
     {
         return self::$rootPath ?: parent::getRootPath();
-    }
-
-    public static function isInstalled($name)
-    {
-        return isset(self::$paths[$name]);
-    }
-
-    public static function getPackages()
-    {
-        return array_keys(self::$paths);
-    }
-
-    public static function getPaths()
-    {
-        $paths = [];
-
-        foreach (self::$paths as $name => $path) {
-            $paths[$name] = self::getRootPath().$path;
-        }
-
-        return $paths;
     }
 }
